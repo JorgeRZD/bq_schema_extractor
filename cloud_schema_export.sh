4e6a39c6a4fbeb89db4320fa8b359e68a436a658
JorgeRZD <jrgortizd@gmail.com>
2025-11-11 12:26:22 -0600

    Include location validation in cloud_schema_export.sh

--- cloud_schema_export.sh
+++ cloud_schema_export.sh
@@ -2,9 +2,18 @@
 GCPproject="$1"
 BQdataset="$2"
 BQtable="$3"
+target="$GCPproject:$BQdataset.$BQtable"
 
-echo "Target table: ${GCPproject}:${BQdataset}.${BQtable}"
+echo "++++ Target table: $target ++++"
+sleep 3
+echo "++++ Checking if table exists ++++"
 
-bq show --schema --format=prettyjson "${GCPproject}:${BQdataset}.${BQtable}">${BQdataset}-${BQtable}.json
+bq show --schema $target>/dev/null
 
-echo "+++ Schema exported to {$PWD} +++"
+if [[ $? -ne 0]]; then 
+    echo "++++ table $target does not exist. Make sure parameters are correct ++++"
+else
+    echo "++++ Table exists. Proceeding to export schema ++++"
+    bq show --schema --format=prettyjson "$target">${BQdataset}-${BQtable}.json
+    echo "+++ Schema exported to {$PWD}/${BQdataset}-${BQtable}.json +++"
+fi
